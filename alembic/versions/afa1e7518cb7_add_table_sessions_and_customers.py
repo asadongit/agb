1cbf17170f1de31fd51668e9cc36c9442935072e
asad <asadweb3.0@gmail.com>
2026-08-08 17:01:31 +0530

    Alembic

--- alembic/versions/afa1e7518cb7_add_table_sessions_and_customers.py
+++ alembic/versions/afa1e7518cb7_add_table_sessions_and_customers.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'afa1e7518cb7'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '000000000000'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
